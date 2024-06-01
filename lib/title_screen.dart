791a0d0ee2e9cad6d18e5b20564e6aa0182af271
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-01 23:19:10 +0200

    Use coco loco for title screen

--- lib/title_screen.dart
+++ lib/title_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flutter/services.dart';
-import 'package:space_kenney/story/coco_script_component.dart';
+import 'package:space_kenney/story/coco_loco_script_component.dart';
 
 import 'core/common.dart';
 import 'core/events.dart';
 
-class TitleScreen extends CocoScriptComponent with KeyboardHandler, TapCallbacks {
+class TitleScreen extends CocoLocoScriptComponent with KeyboardHandler, TapCallbacks {
   @override
   bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
-    if (event case KeyDownEvent it) {
+    if (event case KeyUpEvent it) {
       if (it.logicalKey.keyLabel == ' ') showScreen(Screen.intro);
     }
     return super.onKeyEvent(event, keysPressed);
@@ -23,17 +23,17 @@ class TitleScreen extends CocoScriptComponent with KeyboardHandler, TapCallbacks
 
   @override
   onLoad() async {
-    enact([
-      ['(music music_title.mp3)'],
-      ['(fadeIn (image title_background.png))'],
-      ['(at 0.1 (fadeIn (image title.png 160 5 @topCenter)))'],
-      ['(at 0.1 (fadeIn (image kenny.png 320 256 @bottomRight)))'],
-      ['(at 0.1 (fadeIn (image flame.png 4 245 @bottomLeft)))'],
-      ['(at 0.1 (fadeIn (menuButton "@Settings" 16 200 @topLeft)))'],
-      ['(at 0.1 (fadeIn (menuButton "@Continue" 16 180 @topLeft)))'],
-      ['(at 0.1 (fadeIn (menuButton "@New_Game" 16 160 @topLeft)))'],
-      ['(at 0.1 (pressFireToStart))'],
-    ]);
+    enact('''
+      (music music_title.mp3)
+      (fadeIn (image title_background.png))
+      (at 0.1 (fadeIn (image title.png 160 5 @topCenter)))
+      (at 0.1 (fadeIn (image kenny.png 320 256 @bottomRight)))
+      (at 0.1 (fadeIn (image flame.png 4 245 @bottomLeft)))
+      (at 0.1 (fadeIn (menuButton "@Settings" 16 200 @topLeft)))
+      (at 0.1 (fadeIn (menuButton "@Continue" 16 180 @topLeft)))
+      (at 0.1 (fadeIn (menuButton "@New_Game" 16 160 @topLeft)))
+      (at 0.1 (pressFireToStart))
+    ''');
   }
 
   Vector2 p(double x, double y) => Vector2(x, y);
