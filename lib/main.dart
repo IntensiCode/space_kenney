8d590fe25f3235c42d4035a74783491e1d8a062b
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-04 15:08:34 +0200

    Disable log output for release

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dart_minilog/dart_minilog.dart';
 import 'package:flame/game.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/widgets.dart';
@@ -10,6 +11,8 @@ void main() {
   if (dev) {
     FlameAudio.audioCache.clearAll();
     imageCache.clear();
+  } else {
+    logLevel = LogLevel.none;
   }
   runApp(GameWidget(game: SpaceKenneyGame()));
 }
