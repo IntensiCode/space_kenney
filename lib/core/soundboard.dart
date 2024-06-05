000f71e5583fa3735c227e7c46aa7a81788f0eec
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-05 14:12:49 +0200

    Use player pool in Soundboard

--- lib/core/soundboard.dart
+++ lib/core/soundboard.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:dart_minilog/dart_minilog.dart';
 import 'package:flame_audio/flame_audio.dart';
 
@@ -27,15 +28,56 @@ class Soundboard {
 
   int _activeSounds = 0;
 
-  play(Sound sound, {double? volume}) async {
-    if (_activeSounds >= 10) {
+  play(Sound sound, {double? volume}) {
+    volume ??= soundboard.soundVolume;
+
+    if (_activeSounds >= _maxActive) {
       logWarn('sound overload');
       return;
     }
-    _activeSounds++;
-    volume ??= soundVolume;
-    final player = await FlameAudio.play('${sound.name}.ogg', volume: volume * masterVolume);
-    await player.onPlayerComplete.first;
-    _activeSounds--;
+
+    _playPooled(sound, volume);
   }
+
+  _playPooled(Sound sound, double volume) async {
+    final reuse = _pooledPlayers.indexWhere((it) {
+      if (it.$1 != sound) return false;
+      if (it.$2.state == PlayerState.playing) return false;
+      return true;
+    });
+
+    if (reuse == -1) {
+      while (_pooledPlayers.length >= _maxPooled) {
+        logInfo('disposing pooled player');
+        final (_, player) = _pooledPlayers.removeAt(0);
+        player.dispose();
+      }
+
+      final it = await FlameAudio.play('${sound.name}.ogg', volume: volume * masterVolume);
+      it.setPlayerMode(PlayerMode.lowLatency);
+      it.setReleaseMode(ReleaseMode.stop);
+      it.onPlayerStateChanged.listen((it) {
+        if (it == PlayerState.playing) {
+          _activeSounds++;
+        } else {
+          _activeSounds--;
+        }
+      });
+
+      _activeSounds++;
+      _pooledPlayers.add((sound, it));
+      logInfo('pooled: ${_pooledPlayers.length}');
+    } else {
+      final it = _pooledPlayers.removeAt(reuse);
+      _pooledPlayers.add(it);
+      final player = it.$2;
+      player.setVolume(volume * masterVolume);
+      player.resume();
+    }
+  }
+
+  final _pooledPlayers = <(Sound, AudioPlayer)>[];
+
+  static const _maxActive = 10;
+  static const _maxPooled = 50;
 }
