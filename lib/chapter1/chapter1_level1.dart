f550a55e86709c140031068361ea9b7a48ab310b
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-05 11:23:47 +0200

    Add player to C1L1

--- lib/chapter1/chapter1_level1.dart
+++ lib/chapter1/chapter1_level1.dart
@@ -5,6 +5,7 @@ import 'package:space_kenney/v_shmup/vshmup_moons.dart';
 
 import '../core/common.dart';
 import '../story/direct_script_component.dart';
+import '../v_shmup/vshmup_player.dart';
 import '../v_shmup/vshmup_stars.dart';
 
 class Chapter1_Level1 extends DirectScriptComponent with HasCollisionDetection, KeyboardHandler, TapCallbacks {
@@ -15,8 +16,8 @@ class Chapter1_Level1 extends DirectScriptComponent with HasCollisionDetection, 
     moons();
     at(1, () => subtitles(_kay1, 10, image: kay, audio: 'dialog/c1_l1_kay_1.ogg'));
     at(10.5, () => subtitles(_kenney1, 3, image: kenney, audio: 'dialog/c1_l1_kenney_1.mp3'));
-    // at(10, () => asteroids());
-    asteroids();
+    at(10, () => add(VShmupPlayer()));
+    at(2, () => asteroids());
   }
 
   final _kay1 = 'The asteroid belt ahead is full of minerals and space junk. '
