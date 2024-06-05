e2304b3162e999af771cd869da07b409c1c28fb7
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-05 08:58:48 +0200

    Add basic vshmup player component

---
+++ lib/v_shmup/vshmup_exhaust.dart
@@ -0,0 +1,63 @@
+import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
+import 'package:space_kenney/util/extensions.dart';
+import 'package:space_kenney/util/random.dart';
+
+import '../story/script_functions.dart';
+
+class VShmupExhaust extends PositionComponent with ScriptFunctions {
+  static const count = 10; // TODO depend on speed i guess?
+
+  late final List<SpriteAnimation> _animations;
+  late final List<_ExhaustParticle> _pool;
+
+  double perceivedSpeed = 1;
+
+  @override
+  void onLoad() async {
+    final it = sheet(await image('explosions/explosions.png'), 7, 8);
+    _animations = List.generate(8, (row) => it.createAnimation(row: row, stepTime: 0.05, loop: false));
+    _pool = List.generate(count, (_) => _ExhaustParticle());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (_pool.isNotEmpty) {
+      add(_pool.removeLast()..reset(_animations));
+    }
+    for (final it in children) {
+      final sac = it as _ExhaustParticle;
+      if (sac.animationTicker?.done() != false) _pool.add(sac);
+    }
+    for (final it in _pool) {
+      if (children.contains(it)) it.removeFromParent();
+    }
+  }
+}
+
+class _ExhaustParticle extends SpriteAnimationComponent {
+  double dx = 0;
+  double dy = 10;
+
+  _ExhaustParticle() {
+    anchor = Anchor.center;
+    scale.setAll(0.25);
+  }
+
+  void reset(List<SpriteAnimation> animations) {
+    animation = animations.random(random);
+    animationTicker?.reset();
+    position.x = random.nextDoubleLimit(5) - random.nextDoubleLimit(5);
+    position.y = 18;
+    dx = random.nextDoublePM(25);
+    dy = 75 + random.nextDoublePM(25);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.x += dx * dt;
+    position.y += dy * dt;
+  }
+}
---
+++ lib/v_shmup/vshmup_player.dart
@@ -0,0 +1,115 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:space_kenney/v_shmup/asteroids.dart';
+
+import '../core/common.dart';
+import '../story/script_functions.dart';
+import '../util/input_acceleration.dart';
+import 'vshmup_game_keys.dart';
+
+enum _PlayerState {
+  incoming,
+  playing,
+  //...
+}
+
+class VShmupPlayer extends PositionComponent with ScriptFunctions, KeyboardHandler, VShmupGameKeys, CollisionCallbacks {
+  static final incomingSpeed = dev ? 100 : 20;
+
+  late final xMovement = InputAcceleration(
+    goNegative: () => held[VShmupGameKey.left] == true,
+    goPositive: () => held[VShmupGameKey.right] == true,
+  );
+
+  late final yMovement = InputAcceleration(
+    goNegative: () => held[VShmupGameKey.up] == true,
+    goPositive: () => held[VShmupGameKey.down] == true,
+    position: 40,
+    positionLimit: 80,
+  );
+
+  late SpriteAnimationComponent ship;
+  late SpriteAnimationComponent booster;
+
+  var _state = _PlayerState.incoming;
+
+  @override
+  void onLoad() async {
+    priority = 100;
+
+    final shipFrames = await loadAnimWH('vshmup/player.png', 48, 48);
+    add(ship = makeAnimXY(shipFrames, 0, 0)..playing = false);
+    ship.playing = false;
+
+    final boosterFrames = await loadAnimWH('vshmup/player_small_boosters.png', 16, 10);
+    add(booster = makeAnimXY(boosterFrames, 1, 21));
+
+    final hBox = RectangleHitbox(anchor: Anchor.topCenter);
+    hBox.y += 2;
+    hBox.width = ship.width;
+    hBox.height = ship.height / 4;
+    add(hBox);
+
+    final vBox = RectangleHitbox(anchor: Anchor.bottomCenter);
+    vBox.width = ship.width / 4;
+    vBox.height = ship.height / 3;
+    add(vBox);
+
+    position.x = 160;
+    position.y = 280;
+    anchor = Anchor.center;
+    scale.setAll(0.5);
+
+    _frame = 3;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    switch (_state) {
+      // TODO decelerate
+      case _PlayerState.incoming:
+        if (position.y > 220) {
+          position.y -= dt * incomingSpeed;
+        } else {
+          _state = _PlayerState.playing;
+        }
+
+      case _PlayerState.playing:
+        onPlaying(dt);
+        break;
+    }
+  }
+
+  void onPlaying(double dt) {
+    xMovement.update(dt);
+    yMovement.update(dt);
+    position.x = 160 + xMovement.position;
+    position.y = 180 + yMovement.position;
+
+    final target = xMovement.targetFrame + 3;
+    if (_frameChangeTimer <= 0 && _frame != target) {
+      _frame = target;
+      _frameChangeTimer = 0.25;
+    } else {
+      _frameChangeTimer -= dt;
+    }
+
+    yMovement.targetFrame;
+    booster.scale.setAll(1 - 0.2 * yMovement.targetFrame);
+  }
+
+  int get _frame => ship.animationTicker?.currentIndex ?? 3;
+
+  set _frame(int it) => ship.animationTicker?.currentIndex = it;
+
+  double _frameChangeTimer = 0;
+
+  @override
+  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is Asteroid) {
+      other.onHit(0.5);
+    }
+  }
+}
